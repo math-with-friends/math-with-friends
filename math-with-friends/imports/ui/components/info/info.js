0df0214f191a16673313fead9b3f1890026d04ee
rhynes <31453706+rhynes@users.noreply.github.com>
2017-11-21 23:20:18 -1000

    created basic login/landing page

--- math-with-friends/imports/ui/components/info/info.js
+++ math-with-friends/imports/ui/components/info/info.js
@@ -12,21 +12,4 @@ Template.info.helpers({
   },
 });
 
-Template.info.events({
-  'submit .info-link-add'(event) {
-    event.preventDefault();
 
-    const target = event.target;
-    const title = target.title;
-    const url = target.url;
-
-    Meteor.call('links.insert', title.value, url.value, (error) => {
-      if (error) {
-        alert(error.error);
-      } else {
-        title.value = '';
-        url.value = '';
-      }
-    });
-  },
-});
