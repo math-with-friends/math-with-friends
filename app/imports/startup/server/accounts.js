590af2f54bc693b2eaf6a897aacdd76d15366622
Il Ung <ilungj@gmail.com>
2017-11-26 23:41:00 -1000

    Remove duplicate folder from repository

--- app/imports/startup/server/accounts.js
+++
@@ -1,9 +0,0 @@
-import { Accounts } from 'meteor/accounts-base';
-
-Accounts.onCreateUser(function(options, user) {
-  // For some reason we need to store the additional information in the profile field.
-  user.profile = {};
-  user.profile.gameId = '';
-  user.profile.lobbyId = '';
-  return user;
-});
--- app/imports/ui/pages/landing/landing.js
+++
@@ -1,2 +0,0 @@
-import './landing.html';
-import './landing.css';
