c1416441e983d549cb6a32966ca0a7e771b99534
Il Ung <ilungj@gmail.com>
2017-11-27 00:13:19 -1000

    Implement chat function for channel

--- math-with-friends/imports/startup/server/global.js
+++ math-with-friends/imports/startup/server/global.js
@@ -1,15 +1,11 @@
-import Game from '../../api/game/game.js';
-import Lobby from '../../api/lobby/lobby.js';
-import http from 'http';
-import socket_io from 'socket.io';
 
 // io = null;
 games = {};
 lobbies = {};
 
 Meteor.startup(() => {
-  // new Lobby('test-game');
-  // new Game('test-game');
+
+  // For now, channel behaviors will be defined below.
   const stream = new Meteor.Streamer('test');
   stream.allowRead('all');
 
@@ -24,10 +20,16 @@ Meteor.startup(() => {
     stream.emit('test', lobbiesToSend);
 
     _.each(lobbies, (lobby) => {
-      if (lobby.state == 2 || lobby.state == 3) {
+      if (lobby.state === 2 || lobby.state === 3) {
         lobby.cleanUp();
         delete lobbies[lobby.id];
       }
     });
   }, 3000);
-})
+
+  Meteor.methods({
+    chatChannel(userId, message) {
+      stream.emit('channel-chat', userId, message);
+    }
+  });
+});
--- math-with-friends/imports/ui/pages/channel/channel.js
+++ math-with-friends/imports/ui/pages/channel/channel.js
@@ -3,28 +3,41 @@ import './channel.html';
 import '../game/game.js';
 
 Template.channel.onCreated(function() {
-  this.lobbies = [];
-  this.lobbiesDep = new Tracker.Dependency;
+  this.lobbiesList = [];
+  this.lobbiesListDep = new Tracker.Dependency;
+
+  this.chatList = [];
+  this.chatListDep = new Tracker.Dependency;
+
   this.stream = new Meteor.Streamer('test');
+
   this.stream.on('test', (data) => {
     this.lobbies = _.toArray(data);
-    this.lobbiesDep.changed();
+    this.lobbiesListDep.changed();
+  });
+
+  this.stream.on('channel-chat', (userId, message) => {
+    this.chatList.push({userId: userId, message: message});
+    this.chatListDep.changed();
   });
 });
 
 Template.channel.onDestroyed(function() {
-  console.log('channel onDestroyed');
   if (this.stream) {
     delete Meteor.StreamerCentral.instances['test'];
     this.stream = null;
   }
-})
+});
 
 Template.channel.helpers({
   getLobbies() {
-    Template.instance().lobbiesDep.depend();
-    console.log(Template.instance().lobbies);
-    return Template.instance().lobbies;
+    Template.instance().lobbiesListDep.depend();
+    return Template.instance().lobbiesList;
+  },
+
+  getChatList() {
+    Template.instance().chatListDep.depend();
+    return Template.instance().chatList;
   }
 });
 
@@ -49,5 +62,13 @@ Template.channel.events({
         console.log('Cannot join this lobby.');
       }
     });
-  }
+  },
+
+  'keypress .ui.input input'(event, instance) {
+    const input = $('.ui.input input');
+    if (event.which === 13) {
+      Meteor.call('chatChannel', Meteor.userId(), input.val());
+      input.val('');
+    }
+  },
 });
